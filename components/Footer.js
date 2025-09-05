0e1e16da29e76b15cf80a1a843ab5590436f7e7e
AIWORLDNEXT <jdsaurabh27@gmail.com>
2025-09-05 23:10:28 +0530

    Delete components directory

--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-black text-center p-4 border-t border-gray-700 text-gray-400">
-      © 2025 aiworldnext.com | Your Gateway to AI News, Insights & Careers
-    </footer>
-  );
-}
